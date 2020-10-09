b289affe2819c794bcaa72ed3315550e1c6bde5b
Sergio Hernández Martínez <hm.sergio@gmail.com>
2020-10-09 12:10:32 +0200

    Reference is now an interface to implement

--- src/Api/Domain/Reference.php
+++ src/Api/Domain/Reference.php
@@ -3,24 +3,9 @@ declare(strict_types=1);
 
 namespace DosFarma\Exceptions\Api\Domain;
 
-final class Reference
+interface Reference
 {
-    private string $id;
-    private string $name;
+    public function id(): string;
 
-    public function __construct(string $value, string $name)
-    {
-        $this->id = $value;
-        $this->name = $name;
-    }
-
-    public function id(): string
-    {
-        return $this->id;
-    }
-
-    public function name(): string
-    {
-        return $this->name;
-    }
+    public function name(): string;
 }
